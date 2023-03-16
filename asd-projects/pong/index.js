dcc27e5b71e5f0e7aa74b1495123ce91cbfad875
SlartBart <micahm9848lavca@gmail.com>
2023-03-16 23:08:55 +0000

    Pause screen

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -32,7 +32,7 @@ function runProgram(){
     newMatchScreen : false,
   }
   var spacebar = " ";
-
+  var pause = false;
 
   // Game Item Objects
   function getObject(id, speedx, speedy)
@@ -72,20 +72,21 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
     updateScore();
-    regardingTheBall();
-    movePaddle(player1, paddle1);
-    movePaddle(player2, paddle2);
-    boundCheck(paddle1);
-    boundCheck(paddle2);
-    updatePosition();
+    if(!pause)
+    {
+      regardingTheBall();
+      movePaddle(player1, paddle1);
+      movePaddle(player2, paddle2);
+      boundCheck(paddle1);
+      boundCheck(paddle2);
+      updatePosition();
+    }
     if(player1.score+player2.score>=20)
     {
       endGame();
     }
   }
-  
   /* 
   Called in response to events.
   */
@@ -108,6 +109,10 @@ function runProgram(){
     if(event.key===player2.DOWN)
     {
       player2.DOWNPress = true;
+    }
+    if(event.key===spacebar)
+    {
+      pause=false;
     }
   }
   function stopmove(event)
@@ -143,6 +148,10 @@ function runProgram(){
         ball.speedx=1;
         ball.speedy=1;
       }
+    if(ball.speedx===0)
+    {
+      ball.speedx=-1;
+    }
   }
   function movePaddle(player, paddle)
   {
@@ -183,22 +192,50 @@ function runProgram(){
       {ball.speedy*=1.5;}
       ball.speedy=-1*ball.speedy;
     }
-    /**** ball bounce off paddles ****/
-    /******* ball bounds score *******/
-
   }
   function updateScore()
   {
+    var subplayer;
     if(ball.x<limits.left){
       player2.score+=Math.floor((gameStatus.bounceCount/4)+1);
+      resetBall();
+      subplayer = "player 2 ";
     }
-    else if(ball.x>limits.right){
+    else if(ball.x>limits.right-ball.width){
+      
       player1.score+=Math.floor((gameStatus.bounceCount/4)+1);
+      resetBall();
+      subplayer = "player 1 ";
+    }
+    /* if not paused, don't display pause screen */
+    if(pause)
+    {
+      $("#pause").css("display", "block");
+      $("#player").text(subplayer);
+      $("#points").text(Math.floor((gameStatus.bounceCount/4)+1));
+    }
+    else
+    {
+      $("#pause").css("display", "none");
     }
     //player 2 on left, player 1 on right, and the conditional returns 
     /*insert conditionals for changing score after the ball passes a certain bound*/
     $("#playerscore1").text(player1.score);
     $("#playerscore2").text(player2.score);
+  }
+  function resetBall()
+  {
+    
+    /* wait for spacebar to continue */
+      pause=true;
+    /* reset ball location at random 200 by 200 px , randomize speed */
+    ball.x = Math.floor(Math.random()*200)+285;
+    ball.y = Math.floor(Math.random()*200)+85;
+    ball.speedx = Math.floor(Math.random()*5)-2
+    ball.speedy = Math.floor(Math.random()*5)-2
+    paddle1.y=150;
+    paddle2.y=150;
+    
   }
   function updatePosition()  /* use jquery to move items paddle, and ball */
   {
@@ -210,7 +247,6 @@ function runProgram(){
     $(ball.id).css("top", ball.y + "px");
     $(ball.id).css("left", ball.x + "px");
   }
-  
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
