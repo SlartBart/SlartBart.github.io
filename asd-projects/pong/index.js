dae0cc451d71ae7cf8e53cad5ffbe044fd6cfc11
SlartBart <micahm9848lavca@gmail.com>
2023-03-17 00:05:58 +0000

    commented code for cleanliness

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -17,7 +17,7 @@ function runProgram(){
     score:0,
     UPPress: false,
     DOWNPress: false
-  };
+  };//player2's data and keys active
   var player1 = 
   {
     UP:"w",
@@ -25,14 +25,13 @@ function runProgram(){
     score:0,
     UPPress: false,
     DOWNPress: false
-  };
+  };//player1's data and keys active
   var gameStatus = 
   {
-    bounceCount : 0,
-    newMatchScreen : false,
+    bounceCount : 0 //stores the # of bounces, 
   }
-  var spacebar = " ";
-  var pause = false;
+  var spacebar = " "; //keeps spacebar from being a magic number
+  var pause = false; //stores if screen is paused
 
   // Game Item Objects
   function getObject(id, speedx, speedy)
@@ -46,11 +45,11 @@ function runProgram(){
     object.speedx=speedx;
     object.speedy=speedy;
     return object;
-  }  
-  var ball = getObject("#ball", Math.floor(Math.random()*5)-2, Math.floor(Math.random()*5)-2);/* automatically randomizes the speed of the ball */
-  var paddle1 = getObject("#paddle1", 0, 5);
-  var paddle2 = getObject("#paddle2", 0, 5);
-  var board = getObject("#board", 0, 0);
+  }  //builds out objects
+  var ball = getObject("#ball", Math.floor(Math.random()*5)-2, Math.floor(Math.random()*5)-2);/* automatically randomizes the speed of the ball, creates the ball */
+  var paddle1 = getObject("#paddle1", 0, 5); // creates paddle data
+  var paddle2 = getObject("#paddle2", 0, 5); // creates paddle data
+  var board = getObject("#board", 0, 0); // creates board data
 
 
   var limits = {
@@ -58,7 +57,7 @@ function runProgram(){
     bottom : board.height,
     left : 0,
     right : board.width,
-  }
+  } // used in checking the boarders and goals. 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', startmove);                           // change 'eventType' to the type of event you want to handle
@@ -73,7 +72,7 @@ function runProgram(){
   */
   function newFrame() {
     updateScore();
-    if(!pause)
+    if(!pause) // if not paused, allow normal operations, 
     {
       regardingTheBall();
       movePaddle(player1, paddle1);
@@ -116,7 +115,7 @@ function runProgram(){
     {
       pause=false;
     }
-  }
+  } // checks if a key is pressed to mark it 
   function stopmove(event)
   {
     console.log("depressed");
@@ -145,39 +144,39 @@ function runProgram(){
 
   function keepMoving()/*keeps the ball moving, since there is a chance that the x any y speed get set to zero at gamestart  */
   {
-    if(ball.speedx===0&&ball.speedy===0)
+    if(ball.speedx===0&&ball.speedy===0) //prevents static status
       {
         ball.speedx=1;
         ball.speedy=1;
       }
-    if(ball.speedx===0)
+    if(ball.speedx===0) // prevents unreachable status
     {
       ball.speedx=-1;
     }
   }
-  function movePaddle(player, paddle)
+  function movePaddle(player, paddle) // player and paddle are used for dry code
   {
     if(player.UPPress)
     {
-      paddle.y-=paddle.speedy;
+      paddle.y-=paddle.speedy; // simple! if up move up
     }
     if(player.DOWNPress)
     {
-      paddle.y+=paddle.speedy;
+      paddle.y+=paddle.speedy; // also simple! if down move down
     }
   }
-  function boundCheck(paddle)
+  function boundCheck(paddle) // use of paddle for dry code
   {
     if(paddle.y<limits.top)
     {
-      paddle.y=limits.top;
+      paddle.y=limits.top; //keeps paddle from flying out the top
     }
     if(paddle.y>limits.bottom-paddle.height)
     {
-      paddle.y=limits.bottom-paddle.height;
+      paddle.y=limits.bottom-paddle.height;// heeps paddle from flying out the bottom
     }
   }
-  function regardingTheBall()
+  function regardingTheBall() // defines normal bounce behavior, and animates the ball. 
   {
     keepMoving();
     /***** animate the ball *****/
@@ -197,13 +196,13 @@ function runProgram(){
   }
   function updateScore()
   {
-    var subplayer;
-    if(ball.x<limits.left){
-      player2.score+=Math.floor((gameStatus.bounceCount/4)+1);
+    var subplayer; // stores who won match
+    if(ball.x<limits.left){ // hits left side, player 1 earns point
+      player2.score+=Math.floor((gameStatus.bounceCount/4)+1); // increase score by a fourth of the paddle bounces, with a base of 1
       resetBall();
       subplayer = "player 1 ";
     }
-    else if(ball.x>limits.right-ball.width){
+    else if(ball.x>limits.right-ball.width){ //hits right side, player 2 earns point
       
       player1.score+=Math.floor((gameStatus.bounceCount/4)+1);
       resetBall();
@@ -212,19 +211,21 @@ function runProgram(){
     /* if not paused, don't display pause screen */
     if(pause)
     {
+      /* pause screen code */
       $("#pause").css("display", "block");
       $("#player").text(subplayer);
       $("#points").text(Math.floor((gameStatus.bounceCount/4)+1));
+      gameStatus.bounceCount=0;//reset bounce count, since it only counts each match
+
     }
     else
     {
-      $("#pause").css("display", "none");
+      $("#pause").css("display", "none"); // don't display the screen
     }
     //player 2 on left, player 1 on right, and the conditional returns 
-    /*insert conditionals for changing score after the ball passes a certain bound*/
     $("#playerscore1").text(player1.score);
     $("#playerscore2").text(player2.score);
-    $("#matchPoint").text(Math.floor(gameStatus.bounceCount/4)+1);
+    $("#matchPoint").text(Math.floor(gameStatus.bounceCount/4)+1);//update the current matchvalue always
   }
   function resetBall()
   {
@@ -236,6 +237,7 @@ function runProgram(){
     ball.y = Math.floor(Math.random()*200)+85;
     ball.speedx = Math.floor(Math.random()*5)-2
     ball.speedy = Math.floor(Math.random()*5)-2
+    //resets the position of the paddles
     paddle1.y=150;
     paddle2.y=150;
     
@@ -247,11 +249,12 @@ function runProgram(){
       (ball.x+ball.width>paddle.x)&&
       (ball.y<paddle.y+paddle.height)&&
       (ball.y+ball.height>paddle.y)
+      /* collision detected */
       )
       {
-        ball.speedx*=-1.1;
-        ball.speedy+=Math.floor(Math.random()*4)-2;
-        gameStatus.bounceCount+=1;
+        ball.speedx*=-1.1; // always increase ball speed by 1.1 times its origional, and negative it so the ball turns around
+        ball.speedy+=Math.floor(Math.random()*4)-2;// randomize speed y to make it fun
+        gameStatus.bounceCount+=1;// increase bounce count!!
       }
   }
   function updatePosition()  /* use jquery to move items paddle, and ball */
@@ -266,10 +269,10 @@ function runProgram(){
   }
   function endGame() 
   {
-    $("#WINNER").css("display", "block");
+    $("#WINNER").css("display", "block"); //display endgame text
     if(player1.score>player2.score)
     {
-      $("#WINNER").text("player 1 wins!!!");
+      $("#WINNER").text("player 1 wins!!!"); 
     }
     else if(player1.score<player2.score)
     {
